b8fb14d8881ea35485891b7c803a3928f970eabc
jaeminSon <woalsdnd@gmail.com>
2024-08-15 13:23:03 +0900

    [refactor] data preparation

---
+++ data_preparation/collect_algorithm_list.py
@@ -0,0 +1,63 @@
+import os
+import random
+import requests
+import time
+import json
+
+from tqdm import tqdm
+from bs4 import BeautifulSoup
+
+
+SEARCH_URL = "https://solved.ac/api/v3/search/problem"
+
+
+def collect_problem_list(page: int):
+    querystring = {"query": "", "page": f"{page}"}
+
+    headers = {"Content-Type": "application/json"}
+    response = requests.request("GET", SEARCH_URL, headers=headers, params=querystring)
+
+    summarized_problem_lists = []
+    problem_list = json.loads(response.text).get("items")
+    for prob_desc in problem_list:
+        if "problemId" not in prob_desc:
+            continue
+
+        if "titleKo" not in prob_desc:
+            continue
+
+        # retrieve tags
+        retrieved_tags = []
+        tags = prob_desc.get("tags", [])
+        for tag in tags:
+            displayNames = tag.get("displayNames", [])
+            for display_name in displayNames:
+                if display_name.get("language", "") == "en" and "name" in display_name:
+                    retrieved_tags.append(display_name.get("name"))
+
+        summarized_problem_lists.append(
+            {
+                "problemId": prob_desc["problemId"],
+                "titleKo": prob_desc["titleKo"],
+                "tags": retrieved_tags,
+                "level": prob_desc["level"],
+            }
+        )
+
+    return summarized_problem_lists
+
+
+def read_json(path):
+    with open(path, "r") as f:
+        content = json.load(f)
+    return content
+
+
+if __name__ == "__main__":
+    path_problem_lists = "problem_lists.json"
+    all_problem_lists = []
+    for page in tqdm(range(1, 603)):
+        problem_lists = collect_problem_list(page)
+        all_problem_lists.extend(problem_lists)
+    with open(path_problem_lists, "w", encoding="utf8") as f:
+        json.dump(all_problem_lists, f, indent="\t", ensure_ascii=False)
--- data_preparation/crawl_boj_problems.py
+++ data_preparation/crawl_boj_problems.py
@@ -8,45 +8,6 @@ from tqdm import tqdm
 from bs4 import BeautifulSoup
 
 
-SEARCH_URL = "https://solved.ac/api/v3/search/problem"
-
-
-def crawl_problem_list(page: int):
-    querystring = {"query": "", "page": f"{page}"}
-
-    headers = {"Content-Type": "application/json"}
-    response = requests.request("GET", SEARCH_URL, headers=headers, params=querystring)
-
-    summarized_problem_lists = []
-    problem_list = json.loads(response.text).get("items")
-    for prob_desc in problem_list:
-        if "problemId" not in prob_desc:
-            continue
-
-        if "titleKo" not in prob_desc:
-            continue
-
-        # retrieve tags
-        retrieved_tags = []
-        tags = prob_desc.get("tags", [])
-        for tag in tags:
-            displayNames = tag.get("displayNames", [])
-            for display_name in displayNames:
-                if display_name.get("language", "") == "en" and "name" in display_name:
-                    retrieved_tags.append(display_name.get("name"))
-
-        summarized_problem_lists.append(
-            {
-                "problemId": prob_desc["problemId"],
-                "titleKo": prob_desc["titleKo"],
-                "tags": retrieved_tags,
-                "level": prob_desc["level"],
-            }
-        )
-
-    return summarized_problem_lists
-
-
 def crawl_problem_content(problem_id: int):
     user_agents_list = [
         "Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148",
@@ -100,17 +61,9 @@ if __name__ == "__main__":
     path_problem_lists = "problem_lists.json"
     second_wait_webpage = 2
 
-    # save problem lists to single json file
-    all_problem_lists = []
-    for page in tqdm(range(1, 603)):
-        problem_lists = crawl_problem_list(page)
-        all_problem_lists.extend(problem_lists)
-    with open(path_problem_lists, "w", encoding="utf8") as f:
-        json.dump(all_problem_lists, f, indent="\t", ensure_ascii=False)
-
-    # save problems to files
     dir_save = "problems"
     os.makedirs(dir_save, exist_ok=True)
+
     all_problem_lists = read_json(path_problem_lists)
     for problem_desc in tqdm(all_problem_lists):
         problem_id = problem_desc["problemId"]
